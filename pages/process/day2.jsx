7cd69d2d1b5618c69511133c2f86809ca9777f72
Niklas.Buchfink <niklas.buchfink@web.de>
2022-04-27 12:56:32 +0200

    animation time and drink

--- pages/process/day2.jsx
+++ pages/process/day2.jsx
@@ -1,36 +1,62 @@
+import { motion, AnimatePresence } from "framer-motion"
+
 export default function Day2() { 
   const drinks = [
-    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: "7.5", location: "Dormitory", container: "glass bottle"},
-    {drink: "Coffee", company: "Jakobs", color: "bg-yellow-900", color2: "bg-green-900", amount: "400", time: "8", location: "Dormitory", container: "mug"},
-    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: "12.5", location: "HSLU", container: "glass bottle"},
-    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: "14.5", location: "HSLU", container: "glass bottle"},
-    {drink: "Ginger Ale", company: "Freeway", color: "bg-amber-300", color2: "bg-green-700", amount:"300", time: "16.5", location: "Dormitory", container: "plastic bottle"},
-    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "300", time: "17.5", location: "Dormitory", container: "glass bottle"},
-    {drink: "Mate Vodka", company: "El Tony", color: "bg-amber-400", color2: "bg-blue-800", amount: "300", time: "19", location: "Max Bar", container: "glass bottle"},
-    {drink: "Beer", company: "Super Bock", color: "bg-yellow-500", color2: "bg-white", amount: "300", time: "19.75", location: "Max Bar", container: "glass bottle"},
-    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "400", time: "21.75", location: "Parterre Bar", container: "glass"},
-    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "500", time: "22.16", location: "Parterre Bar", container: "glass"},
-    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "500", time: "23.33", location: "Bar 59", container: "glass"},
+    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: 7.5, location: "Dormitory", container: "glass bottle"},
+    {drink: "Coffee", company: "Jakobs", color: "bg-yellow-900", color2: "bg-green-900", amount: "400", time: 8, location: "Dormitory", container: "mug"},
+    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: 12.5, location: "HSLU", container: "glass bottle"},
+    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "200", time: 14.5, location: "HSLU", container: "glass bottle"},
+    {drink: "Ginger Ale", company: "Freeway", color: "bg-amber-300", color2: "bg-green-700", amount:"300", time: 16.5, location: "Dormitory", container: "plastic bottle"},
+    {drink: "Sparkling Water", company: "SodaStream", color: "bg-cyan-200", color2: "bg-sky-500", amount: "300", time: 17.5, location: "Dormitory", container: "glass bottle"},
+    {drink: "Mate Vodka", company: "El Tony", color: "bg-amber-400", color2: "bg-blue-800", amount: "300", time: 19, location: "Max Bar", container: "glass bottle"},
+    {drink: "Beer", company: "Super Bock", color: "bg-yellow-500", color2: "bg-white", amount: "300", time: 19.75, location: "Max Bar", container: "glass bottle"},
+    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "400", time: 21.75, location: "Parterre Bar", container: "glass"},
+    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "500", time: 22.16, location: "Parterre Bar", container: "glass"},
+    {drink: "Beer", company: "Eichhof", color: "bg-yellow-500", color2: "bg-white", amount: "500", time: 23.33, location: "Bar 59", container: "glass"},
   ]
+
+  const variants = {
+    hidden: {
+      opacity: 0,
+      translateY: 80,
+    },
+  }
 
   return(
     <div>
       <h1 className="mx-auto my-64 w-fit">Drinks of the day</h1>
       <div className="mx-auto w-fit">
         <ul className="flex flex-col gap-32 text-md font-bold color-teal-400 my-64">
-          {drinks.map((item,index) =>
-            <li key={index}>
-              <div className={`p-4 rounded-t-md ${item.color2}`}>
-                <div className="text-stone-900 text-center">{item.company}</div>
-              </div>
-              <div className={`flex flex-col text-stone-900 px-8 py-4 rounded-b-md ${item.color}`}>
-                <span className="text-black text-xl mb-4">{item.drink}</span>
-                <span>Amount: {item.amount}</span>
-                <span>Container: {item.container}</span>
-                <span>Location: {item.location}</span>
-              </div>
-            </li>
-          )}
+          <AnimatePresence exitBeforeEnter>
+            {drinks.map((item,index) =>
+              <motion.li 
+                key={index}
+                variants={variants}
+                initial="hidden"
+                animate={{
+                  opacity: 1,
+                  translateY: 0, 
+                  transition: {delay: item.time-6 }
+                }}
+                whileHover={{ rotate: -35}}
+                exit="hidden"
+                transition={{
+                  duration: 0.4,
+                  ease: "easeOut"
+                }}
+              >
+                <div className={`p-4 rounded-t-md ${item.color2}`}>
+                  <div className="text-stone-900 text-center">{item.company}</div>
+                </div>
+                <div className={`flex flex-col text-stone-900 px-8 py-4 rounded-b-md ${item.color}`}>
+                  <span className="text-black text-xl mb-4">{item.drink}</span>
+                  <span>Amount: {item.amount}</span>
+                  <span>Container: {item.container}</span>
+                  <span>Location: {item.location}</span>
+                </div>
+              </motion.li>
+            )}
+          </AnimatePresence>
         </ul>
       </div>
     </div>
